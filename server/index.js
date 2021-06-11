16756a749763eb7ce21662dcb1e9ff38cc48157d
nfcarbone <nfcarbone@gmail.com>
2021-06-11 10:32:02 -0400

    api blueprint

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+require('dotenv').config();
+
 const app = express();
 
 // Serve React App
@@ -10,7 +12,39 @@ app.get('/', (_, res) => {
   res.sendFile(path.join(buildFolder, 'index.html'));
 });
 
-app.get('/test', (_, res) => res.send('success!'));
+app.get('/auth', (req, res) => {
+  // generate state string
+  // store state in cookie
+  // get auth url (pass in state string)
+  // redirect client to github auth endpoint
+});
+
+app.get('/auth/callback', (req, res) => {
+  // if req.query.error, status 400, return redirect to login
+  // parse cookies for state and access_token
+  // if neither, same ⬆
+  // validate req.query.state === state
+  // if not, same ⬆
+  // set access_token cookie
+  //
+  // redirect user to dashboard
+});
+
+const ensureAuthenticated = (req, res, next) => {
+  // parse cookies for access_token
+  // if none, redirect to login
+  // get user from GitHub
+  // if fail, redirect to login
+  // else call next()
+};
+
+app.get('/user/repos', ensureAuthenticated, (req, res) => {
+  // parse cookies for access_token
+  // get repos from GitHub
+  // get scores for repos
+  // get badges for repos
+  // send list of repos with scores and badges
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
--- server/requests.js
+++ server/requests.js
@@ -1,22 +1,36 @@
-// Authorization Request URL 👇 (Will return 'code' for exchange and 'state' for verification)
-const getAuthURL = (state) =>
+const axios = require('axios');
+
+// Authorization Request URL (Will return 'code' for exchange and 'state' for verification)
+module.exports.getAuthURL = (state) =>
   `https://github.com/login/oauth/authorize?\
 client_id=0oaxebd0w120PQAvr5d6&\
 scope=read%3Auser%20repo&\
 state=${state}`;
 
-// Token Request 👇
-const getToken = (code) =>
-  axios.post(
-    'https://github.com/login/oauth/access_token',
-    {
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      code,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+module.exports.axios = {
+  getToken: (code) =>
+    axios.post(
+      'https://github.com/login/oauth/access_token',
+      {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code,
       },
-    }
-  );
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    ),
+
+  getUserRepos: (username, accessToken) =>
+    axios.get(`https://api.github.com/users/${username}/repos`, {
+      headers: {
+        authorization: `bearer ${accessToken}`,
+        accept: 'application/vnd.github.v3+json',
+      },
+      params: {
+        type: 'all',
+      },
+    }),
+};
